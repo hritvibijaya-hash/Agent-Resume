f9be9d935b488fe6dddfe22aec3c98651bd33bda
hritvibijaya-hash <hritvibijaya@gmail.com>
2026-07-31 01:50:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -237,7 +237,13 @@ Build ATS Friendly Resume • Latest Jobs • AI Powered
 
 #=============================================================================================Image===========================================================================
 
-st.image("bg.png")
+col1, col2, col3 = st.columns([2,4,2])
+
+with col2:
+    st.image(
+        "bg.png",
+        use_container_width=True
+    )
 
 
 
